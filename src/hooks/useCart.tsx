34beb96826e163e724da510fdf0f2695bd6bf71a
kleuvys <kleuvys@gmail.com>
2022-02-18 09:24:15 -0300

    commiting all changes to repository

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -23,20 +23,33 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
-    } catch {
-      // TODO
+      const isProductOnCart = cart.filter(item => item.id === productId);
+
+      if (isProductOnCart) {
+        //if a bug cause cart having multiple products with same id, all of them will 
+        //be updated in amount on cart
+        isProductOnCart.map(item => item.amount++);
+        const newCart = [...cart, cart[]]
+        setCart([...cart, ...isProductOnCart]);
+        localStorage.setItem('@RocketShoes', JSON.stringify([...cart, ...isProductOnCart]));
+      } else {
+        const product = await api.get<Product>(`products/${productId}`);
+
+        setCart([...cart, product])
+      }
+    } catch (e) {
+      console.log(e);
     }
   };
 
